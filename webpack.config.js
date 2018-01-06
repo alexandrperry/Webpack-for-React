6d00ee23b3482d71e182748696de0369bdc1074d
Alexandr Chernyakov <chernyakovit@gmail.com>
2018-01-06 02:59:16 +0200

    add react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         loaders: [{
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
+            loader: ['react-hot-loader','babel-loader']
         },
             {
                 test: /\.css$/,
